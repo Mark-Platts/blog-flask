51ae7f3d57c3dc41a45890dc56b1e4a0e0414270
Mark-Platts <markplattstutoring@outlook.com>
2021-04-24 21:58:22 +0100

    structure working

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+import os
+
+#Logic Stuff
+def getArcList():
+    mypath = "./templates"
+    hold_dir = os.listdir(mypath)
+    dontUse = ['archive.html', 'base.html', 'home.html']
+    arcList = []
+    for i in hold_dir:
+        if i not in dontUse:
+            arcList.append(i)
+    return(arcList)
+
+
+
+
+#Server Stuff
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,6 +22,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return(render_template('home.html'))
+
+@app.route("/archive")
+def archive():
+    arcList = getArcList()
+    return(render_template('archive.html', arcList = arcList))
+
+@app.route('/<path:page>')
+def goTo(page):
+    return render_template('%s.html' % page)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
